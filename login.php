2ae60236faaf5ce6da5df7685b7f1a762e865643
pihhuang <dreammyth9892@gmail.com>
2015-04-24 23:11:36 +0800

    login html only

---
+++ login.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>登入介面</title>
+</head>
+<body>
+    <form method="POST" action="login_act.php">
+        <p> 帳戶 <input name="accout" /></p>
+        <p> 密碼 <input name="password" /></p>
+        <input type="submit" />
+    </form>
+</body>
+</html>
